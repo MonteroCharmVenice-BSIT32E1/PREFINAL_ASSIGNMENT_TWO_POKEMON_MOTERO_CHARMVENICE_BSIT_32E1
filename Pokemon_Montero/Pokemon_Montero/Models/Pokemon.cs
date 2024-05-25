25d736dcf9da9b4fa9f13a4a60fc6b8efc2efcd1
Montero Charm Venice <charmvenice@gmail.com>
2024-05-25 16:14:29 +0800

    added code for model class

---
+++ Pokemon_Montero/Pokemon_Montero/Models/Pokemon.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace Pokemon_Montero.Models
+{
+    public class Pokemon
+    {
+        public string Name { get; set; }
+        public List<string> Abilities { get; set; }
+        public List<string> Moves { get; set; }
+    }
+}
--- Pokemon_Montero/Pokemon_Montero/obj/Debug/net8.0/Pokemon_Montero.AssemblyInfo.cs
+++ Pokemon_Montero/Pokemon_Montero/obj/Debug/net8.0/Pokemon_Montero.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Pokemon_Montero")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c0c2ca83f45eb12214e86f37b58ebebc9c4301c4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a09059e7476be90901e12bc463705e891ec67581")]
 [assembly: System.Reflection.AssemblyProductAttribute("Pokemon_Montero")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Pokemon_Montero")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
