8775a57649baa5c17bf562dfd6adba508e3ea83c
Montero Charm Venice <charmvenice@gmail.com>
2024-05-25 16:16:12 +0800

    installed System.Net.Http

--- Pokemon_Montero/Pokemon_Montero/obj/Debug/net8.0/Pokemon_Montero.AssemblyInfo.cs
+++ Pokemon_Montero/Pokemon_Montero/obj/Debug/net8.0/Pokemon_Montero.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Pokemon_Montero")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+25d736dcf9da9b4fa9f13a4a60fc6b8efc2efcd1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+78bad10c690a8ec4a9aebd09e4fae0aea6ede325")]
 [assembly: System.Reflection.AssemblyProductAttribute("Pokemon_Montero")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Pokemon_Montero")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
